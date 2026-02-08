0dacae093b715ed327b6c619ac337de3e422ce7a
luca <luca@gatty.dev>
2026-02-08 17:22:39 +0100

    first commit

---
+++ app/api/route.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  other: {
+    "base:app_id": "6988b1b373cda529e5cd6789",
+  },
+};
+
+export default function Home() {
+  return <div>{/* Your page content */}</div>;
+}
